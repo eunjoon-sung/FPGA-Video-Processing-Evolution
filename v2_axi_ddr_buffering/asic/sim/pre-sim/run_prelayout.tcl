0d59bb407050aa89d4018f88b33f046fb08b8661
Sung Eunjoon <1225seong@gmail.com>
2026-02-27 17:32:21 +0900

    Update run_prelayout.tcl

--- v2_axi_ddr_buffering/asic/sim/pre-sim/run_prelayout.tcl
+++ v2_axi_ddr_buffering/asic/sim/pre-sim/run_prelayout.tcl
@@ -1,10 +1,10 @@
 //run_prelayout.tcl
 
 # netlist file
-/home/s_2025_h007/PROJECT/HDMI_DDR/syn/outputs/AXI_writer_netlist.v
+/home/s_2025_h007/PROJECT/HDMI_DDR/syn/outputs/AXI4_writer_netlist.v
 
 # tb file
-/home/s_2025_h007/PROJECT/HDMI_DDR/sim/pre-sim/tb_axi_writer_gls.v
+/home/s_2025_h007/PROJECT/HDMI_DDR/sim/pre-sim/tb_axi4_writer_gls.v
 
 #library file
 /home/s_2025_h007/PROJECT/HDMI_DDR/edu_lib/verilog/slow_vdd1v0_basicCells.v
